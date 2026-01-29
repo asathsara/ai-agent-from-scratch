33a33a813f605f51f997f8bbf970f937e5fcd5b4
A S <65203960+asathsara@users.noreply.github.com>
2026-01-29 17:42:42 +0530

    feat: add logging for token usage metadata.

--- main.py
+++ main.py
@@ -2,14 +2,23 @@ import os
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-api_key = os.environ.get("GEMINI_API_KEY")
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="How does AI work?",
+    )
+    print(response.text)
 
-client = genai.Client(api_key=api_key)
+    if response is None or response.usage_metadata is None:
+        print("Usage metadata not available")
+    else:
+        print(f"Prompt Tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response Tokens: {response.usage_metadata.candidates_token_count}")
+        print(f"Total Tokens: {response.usage_metadata.total_token_count}")
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="How does AI work?",
-)
-print(response.text)
+if __name__ == "__main__":
+    main()

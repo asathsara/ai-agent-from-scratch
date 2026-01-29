25739d797e05d9a2c1a90964be21c5d0761885bc
A S <65203960+asathsara@users.noreply.github.com>
2026-01-29 18:09:04 +0530

    feat: implment verbose flag for printing tokens

--- main.py
+++ main.py
@@ -20,22 +20,32 @@ def main():
         sys.exit(1)
     prompt = sys.argv[1]
 
+    verbose_flag = False
+    if len(sys.argv) > 2:
+        if sys.argv[2] == "--verbose":
+            verbose_flag = True
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)])
+    ]
+
     with console.status("[bold green]Generating response...[/bold green]"):
         response = client.models.generate_content(
             model="gemini-2.5-flash",
             config=types.GenerateContentConfig(
                 system_instruction="You are a helpful assistant. Answer as a markdown format. Be concise and friendly."),
-            contents=prompt,
+            contents=messages,
         )
 
     if response.text:
         console.print(Markdown(response.text))
     
-    console.print("\n" + "-" * 20)
-    if response is None or response.usage_metadata is None:
-        console.print("[yellow]Usage metadata not available[/yellow]")
-    else:
-        metadata = response.usage_metadata
+    if verbose_flag:
+        console.print("\n" + "-" * 20)
+        if response is None or response.usage_metadata is None:
+            console.print("[yellow]Usage metadata not available[/yellow]")
+        else:
+            metadata = response.usage_metadata
         console.print(Panel(
             f"[bold blue]Prompt Tokens:[/bold blue] {metadata.prompt_token_count}\n"
             f"[bold blue]Response Tokens:[/bold blue] {metadata.candidates_token_count}\n"
